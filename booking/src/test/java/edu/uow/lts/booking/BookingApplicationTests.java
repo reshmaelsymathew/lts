c5081d12c34b1e83caa9a461b2237a45caef0b52
reshmamathew8 <reshmamathew8@gmail.com>
2024-09-23 01:59:23 +1000

    initial commit

---
+++ booking/src/test/java/edu/uow/lts/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uow.lts.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
